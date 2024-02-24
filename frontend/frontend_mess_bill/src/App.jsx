b978d495aee98df141d6960efd6b40099b1a3017
vighnesh2709 <bvighnesh27@gmail.com>
2024-02-24 15:26:01 +0530

    more styling

--- frontend/frontend_mess_bill/src/App.jsx
+++ frontend/frontend_mess_bill/src/App.jsx
@@ -13,8 +13,8 @@ function App() {
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                <button onClick={() => { window.location.href = "/" }} className="bg-gray-900 text-green-300 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Landing</button>
+                {/* Current: "bg-gray-700 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                <button onClick={() => { window.location.href = "/" }} className="text-green-300 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Landing</button>
                 <button onClick={() => { window.location.href = "/Indv" }} className="text-green-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Check Bill</button>
                 <button onClick={() => { window.location.href = "/Leave" }} className="text-green-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Leave</button>
                 <button onClick={() => { window.location.href = "/Monthy Table" }} className="text-green-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Monthy Table</button>
@@ -22,9 +22,6 @@ function App() {
             </div>
           </div>
         </div>
-      </div>
-      {/* Mobile menu, show/hide based on menu state. */}
-      <div className="sm:hidden" id="mobile-menu">
       </div>
     </nav>
 
@@ -59,15 +56,40 @@ function IndividualBill({Cost,SetCost}){
   useEffect(()=>{
 
   },[Cost])
-  return<div>
-    <input id="name" type='text'placeholder='Name'></input>
-    <input id="id" type='text' placeholder='ID'></input>
-    <br></br>
-    <button onClick={calculate}>calculate</button>
-    <br></br>
-    {Cost}
-    
-  </div>
+ return (
+  
+    <div style={{ backgroundColor: '#4a5568', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center' }}>
+      <div className="flex w-72 flex-col items-start gap-2 text-green-300">
+        <label style={{ display: 'block', fontSize: '0.875rem', fontWeight: '600', textAlign: 'left' }} htmlFor="name">Name</label>
+        <input id="name" type='text' placeholder='Name' size="md" className='border-2 border-green-200' />
+      
+        <label style={{ display: 'block', fontSize: '0.875rem', fontWeight: '600', textAlign: 'left' }} htmlFor="id">ID</label>
+        <input id="id" type='text' placeholder='ID' size="md" className='border-2 border-green-200' />
+      
+        <br />
+        <button onClick={calculate} className='bg-gray-800 py-2 px-2 rounded-md border-2 border-green-100'>calculate</button>
+        <br />
+        <div style={{position: 'absolute',left: "50%",top:"50%",transform: "translate(-50%,-100%)", width:"300px",height:"200px",background: "white"}}>
+          <div className='text-black'>
+            No of days: {Cost/200}
+            <br></br>
+            Total:{Cost}
+            <br></br>
+            GST:{Cost*0.18}
+            <br></br>
+            ______________________________________
+            <br></br>
+            Total:{Cost+(Cost*0.18)}
+
+          </div>
+          
+
+        </div>
+      </div>
+    </div>
+);
+
+  
   async function calculate() {
   const name = document.getElementById("name").value;
   const id = document.getElementById("id").value;
@@ -84,15 +106,20 @@ function IndividualBill({Cost,SetCost}){
 
 }
 function Leave(){
-  return<div>
-    <input type='number'id='id'></input>
-    <div>From</div>
-    <input type='date' id='Start_date' ></input>
-    <div>Till</div>
-    <input type='date' id='End_date'></input>
+  return(<div style={{background:"#4a5568",minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center'}}>
+    <div className='text-green-300'>
+    <label style={{ display: 'block', fontSize: '0.875rem', fontWeight: '600', textAlign: 'left' }} htmlFor="id">ID</label>
+    <input type='number'id='id' className='text-black rounded-md'></input>
+    <label style={{ display: 'block', fontSize: '0.875rem', fontWeight: '600', textAlign: 'left' }} htmlFor="Start_date">From</label>
+    <input type='date' id='Start_date' className='text-black rounded-md' ></input>
+    <label style={{ display: 'block', fontSize: '0.875rem', fontWeight: '600', textAlign: 'left' }} htmlFor="Start_date" >Till</label>
+    <input type='date' id='End_date' className='text-black rounded-md'></input>
     <br></br>
-    <button onClick={calculate}>Confirm</button>
-  </div>
+    <br></br>
+    <button onClick={calculate} className='bg-gray-800 py-2 px-2 rounded-md border-2 border-green-100'>Confirm</button></div>
+  </div>)
+  
+  
   async function calculate(){
       let start=document.getElementById("Start_date").value;
       let end=document.getElementById("End_date").value;
